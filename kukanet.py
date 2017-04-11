76fe1bd8afb7abba032109fba9ec25e6c1a4ad02
Davide Rosa <d.rosa@imts.eu>
2017-04-11 14:49:56 +0200

    Some fixes.

--- kukanet.py
+++ kukanet.py
@@ -1,4 +1,6 @@
 import win32pipe, win32file
+import win32security
+import pywintypes
 
 #http://stackoverflow.com/questions/13319679/createnamedpipe-in-python
 #https://msdn.microsoft.com/en-us/library/windows/desktop/aa365150(v=vs.85).aspx
@@ -13,12 +15,16 @@ f.flush()
 f.close()
 
 """
+pipename = r'\\.\pipe\kukanet'
 
-p = win32pipe.CreateNamedPipe(r'\\.\pipe\kukanet',
+sa = win32security.SECURITY_ATTRIBUTES()
+sa.SetSecurityDescriptorDacl(1, None, 0)
+
+p = win32pipe.CreateNamedPipe(pipename,
     win32pipe.PIPE_ACCESS_DUPLEX,
     win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_WAIT,
-    1, 65536, 65536,300,None)
-
+    win32pipe.PIPE_UNLIMITED_INSTANCES, 65536, 65536, 6000, sa)
+    
 win32pipe.ConnectNamedPipe(p, None)
 
 #data = b"Hello Pipe"  
